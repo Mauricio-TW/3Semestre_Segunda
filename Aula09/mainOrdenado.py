88f3f6bca39828ab129c43fb4a34e037f0bb858b
Mauricio-TW <115851331+Mauricio-TW@users.noreply.github.com>
2025-06-02 21:31:41 -0300

    Aula 02/06

---
+++ Aula09/mainOrdenado.py
@@ -0,0 +1,10 @@
+from ListaEncadeadaOerdenada import ListaEncadeadaOrdenada
+
+lista = ListaEncadeadaOrdenada()
+lista.imprimir()
+
+lista.add("Julia")
+lista.add("Pedro")
+lista.add("João")
+lista.add("Maria")
+lista.add("Carlos")
